18156ba26d284c4b17a2f5fd02ff36deaaed3516
Eduardaribeiro29 <dudarnascimento@hotmail.com>
2026-05-07 00:32:59 +0000

    atualização index

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-import express from "express"
-import cors from "cors"
-import router from "./src/routes/usuarios.routes.js"
+import express from "express";
+import cors from "cors";
+import usuarios.routes from "./src/routes/usuarios.routes.js";
+import tarefas.routes from "./src/routes/tarefas.routes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/usuarios", routerUsuarios);
+app.use("/usuarios", usuarios.routes);
+app.use("/tarefas", tarefas.routes);
+
+app.get('/', (req, res) => {
+    res.json({
+        status: 'ok',
+        recursos: ['/usuarios', '/tarefas']
+    });
+});
 
 const PORTA = 3000;
 app.listen(PORTA, () => {
